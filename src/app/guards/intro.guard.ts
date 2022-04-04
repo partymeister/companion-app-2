249a54f152608c36dd9c18b76c00116439c97e31
Lukas Roth <lukas.roth252@gmail.com>
2022-04-04 22:44:50 +0200

    finished intro screen

---
+++ src/app/guards/intro.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { Observable } from 'rxjs';
+import {StorageService} from "../services/storage.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IntroGuard implements CanActivate {
+  constructor(private storageService: StorageService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean|UrlTree>|Promise<boolean|UrlTree>|boolean|UrlTree {
+    return this.storageService.get('intro_visited').then((introVisited: boolean) => {
+      if (introVisited) {
+        return true;
+      }
+      return this.router.navigate(['intro']).then(r => r);
+    });
+  }
+}
