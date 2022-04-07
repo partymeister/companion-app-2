4e365f7c706b25e9823596964be9080b4db892b1
Reza Esmaili <bitbucket@dfox.info>
2022-04-07 16:54:15 +0200

    Add close button to barcode scanner

--- src/app/services/barcode.service.ts
+++ src/app/services/barcode.service.ts
@@ -1,37 +1,77 @@
-import { Injectable } from '@angular/core';
-import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
-import { Device } from '@capacitor/device';
+import {Injectable} from '@angular/core';
+import {BarcodeScanner} from '@capacitor-community/barcode-scanner';
+import {Device} from '@capacitor/device';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BarcodeService {
 
-  constructor() { }
+  constructor() {
+  }
 
   startScan = async () => {
     const info = await Device.getInfo();
 
     if (info.platform === 'android') {
       const granted = await this.hasPermission();
-      if (!granted) {return;}
+      if (!granted) {
+        return;
+      }
     }
-    document.body.style.opacity='0';
-    document.body.style.background = 'transparent';
-    BarcodeScanner.hideBackground();
+
+    this.hideBackground();
+
     const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
 
     // if the result has content
     if (result.hasContent) {
       console.log(result.content); // log the raw scanned content
-      document.body.style.background = '';
-      document.body.style.opacity='1';
+      this.showBackground();
       return result.content;
     }
   };
 
+  showBackground() {
+    const appRoot = document.getElementsByTagName('app-root')[0] as HTMLElement;
+    appRoot.style.opacity = '1';
+    document.body.style.background = '';
+
+    document.getElementById('bc-close').remove();
+
+    BarcodeScanner.showBackground();
+  }
+
+  hideBackground() {
+    const appRoot = document.getElementsByTagName('app-root')[0] as HTMLElement;
+    appRoot.style.opacity = '0';
+    document.body.style.background = 'transparent';
+
+    const node = document.createElement("div");
+    node.id = 'bc-close';
+    node.style.background = 'rgb(229, 85, 74)';
+    node.style.position = 'absolute';
+    node.style.width = '100%';
+    node.style.height = '100px';
+    node.style.opacity = '1';
+    node.style.textAlign = 'center';
+    node.style.verticalAlign = 'middle';
+    node.style.paddingTop = '65px';
+
+    const textnode = document.createTextNode("Close");
+    node.appendChild(textnode);
+
+    node.addEventListener('click', () => {
+      this.stopScan();
+    });
+
+    document.getElementById('barcodeScannerCloseButton').appendChild(node);
+
+    BarcodeScanner.hideBackground();
+  }
+
   stopScan = async () => {
-    await BarcodeScanner.showBackground();
+    this.showBackground();
     await BarcodeScanner.stopScan();
   };
 
